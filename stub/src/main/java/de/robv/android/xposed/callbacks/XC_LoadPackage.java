485cccfee4b20a94155cf4eb37371ade85d5d989
墨 <129926395+mohuanmo@users.noreply.github.com>
2026-08-02 19:08:29 +0800

    Add stub: stub/src/main/java/de/robv/android/xposed/callbacks/XC_LoadPackage.java

---
+++ stub/src/main/java/de/robv/android/xposed/callbacks/XC_LoadPackage.java
@@ -0,0 +1,9 @@
+package de.robv.android.xposed.callbacks;
+
+public abstract class XC_LoadPackage {
+    public static final class LoadPackageParam {
+        public String packageName;
+        public ClassLoader classLoader;
+        public boolean isFirstApplication;
+    }
+}
