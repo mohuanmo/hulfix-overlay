56bad190221602bb52f74eb9f364536053e432bf
墨 <129926395+mohuanmo@users.noreply.github.com>
2026-08-03 20:02:43 +0800

    v22-fix: expand stub XposedBridge - add hookAllConstructors

--- stub/src/main/java/de/robv/android/xposed/XposedBridge.java
+++ stub/src/main/java/de/robv/android/xposed/XposedBridge.java
@@ -1,11 +1,17 @@
 package de.robv.android.xposed;
 
+import java.lang.reflect.Constructor;
+
 public final class XposedBridge {
     public static void log(String text) {
         android.util.Log.i("Xposed", text);
     }
 
     public static XC_MethodHook.Unhook[] hookAllMethods(Class<?> clazz, String methodName, XC_MethodHook callback) {
+        return null;
+    }
+
+    public static XC_MethodHook.Unhook[] hookAllConstructors(Class<?> clazz, XC_MethodHook callback) {
         return null;
     }
 }
