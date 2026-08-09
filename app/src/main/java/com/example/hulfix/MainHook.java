c339a9b64305eed31be2e6fc99e24d1625e7fbaa
mohuanmo <kxxn_0623@qq.com>
2026-08-09 14:19:24 +0000

    fix: add missing android.os.Parcelable import to resolve CI build failure

--- app/src/main/java/com/example/hulfix/MainHook.java
+++ app/src/main/java/com/example/hulfix/MainHook.java
@@ -19,6 +19,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.os.Parcelable;
 import android.os.SystemClock;
 import android.service.notification.StatusBarNotification;
 import android.view.Gravity;
